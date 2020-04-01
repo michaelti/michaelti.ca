db19fdc5e60de8eaf99bfdcb7ecb13b8eb4c2a5a
Michael <m@michaelti.ca>
2020-03-31 23:35:23 -0400

    move scripts to own files

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -1,9 +1,49 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-    mediumZoom('.post-content img', {
-        margin: 15,
-        background: 'rgba(247, 247, 247, 0.5)',
-        scrollOffset: 0
-    });
+    // Apply medium zoom effect to images in post content
+    (function() {
+        mediumZoom('.post-content img', {
+            margin: 15,
+            background: 'rgba(247, 247, 247, 0.5)',
+            scrollOffset: 0
+        });
+    })();
+
+    // Typewriter effect for home page
+    (function() {
+        const element = document.getElementById('typewriter');
+        if (!element) return;
+        
+        const text = element.firstElementChild.innerText;
+
+        var spanOne = document.createElement('span');
+        var spanTwo = document.createElement('span');
+
+        spanOne.innerText = '';
+        spanTwo.innerText = text;
+        spanTwo.style.visibility = 'hidden';
+
+        element.removeChild(element.firstElementChild);
+        element.appendChild(spanOne);
+        element.appendChild(spanTwo);
+
+
+        var i = 0;
+        var speed = 20;
+        
+        function typeWriter() {
+            if (i <= text.length) {
+                spanOne.innerText = text.slice(0, i);
+                spanTwo.innerText = text.slice(i);
+                setTimeout(typeWriter, speed);
+                i++;
+            } else if (i > text.length) {
+                //Done, clean up
+                element.removeChild(spanTwo);
+            }
+        }
+
+        typeWriter();
+    })();
 
 });
