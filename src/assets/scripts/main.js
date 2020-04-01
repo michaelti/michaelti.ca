116e6a96350c2fa8401390774d6aa738970919b5
Michael <m@michaelti.ca>
2020-03-31 23:24:34 -0400

    move scripts to own files

---
+++ src/assets/scripts/main.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+    mediumZoom('.post-content img', {
+        margin: 15,
+        background: 'rgba(247, 247, 247, 0.5)',
+        scrollOffset: 0
+    });
+
+});
